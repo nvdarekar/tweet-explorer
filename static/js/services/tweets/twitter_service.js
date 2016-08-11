4297debbe0d535a9e3abea1e9aae6aa7cea70360
Nitin Darekar <nvdarekar@gmail.com>
2016-08-11 22:38:12 +0530

    Change twitter proxy server url used in twitter service

--- static/js/services/tweets/twitter_service.js
+++ static/js/services/tweets/twitter_service.js
@@ -13,7 +13,7 @@ app.factory('TwitterService',
      * @returns promise of tweets json
      */
     var fetchTweets = function (screen_name, tweetCount) {
-        var url = "http://localhost:7890/1.1/statuses/user_timeline.json?count="+tweetCount.toString()+"&screen_name=" + screen_name;
+        var url = "https://glacial-fortress-72105.herokuapp.com/1.1/statuses/user_timeline.json?count="+tweetCount.toString()+"&screen_name=" + screen_name;
         return $http.get(url)
                 .then(function(response) {
                     return $q.resolve(response.data)
